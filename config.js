0077ca57d8b4801aca72a30ea3a8b59f4ea80a9f
SDemianyk02 <serhii.demianyk@nocoderebels.dev>
2025-05-19 09:21:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 export const config = {
-  domainSource: "https://f69a71f6-9fd8-443b-a040-78beb5d404d4.weweb-preview.io", // Your WeWeb app preview link
+  domainSource: "https://www.heyanon.io", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/event/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta"
-      },
-      {
-          pattern: "/team/profile/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
+          pattern: "/?post=+",
+          metaDataEndpoint: "https://xsrr-l2ye-dpbj.f2.xano.io/api:Coq7oZJp/posts/get_single/{post_id}"
       }
+      // {
+      //     pattern: "/team/profile/[^/]+",
+      //     metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
+      // }
       // Add more patterns and their metadata endpoints as needed
   ]
 };
