43f894a92f3e7e49f077a8148706d023ba299e5b
SDemianyk02 <serhii.demianyk@nocoderebels.dev>
2025-05-19 09:22:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ export const config = {
       {
           pattern: "/?post=+",
           metaDataEndpoint: "https://xsrr-l2ye-dpbj.f2.xano.io/api:Coq7oZJp/posts/get_single/{post_id}"
+      },
+      {
+          pattern: "/post/[^/]+",
+          metaDataEndpoint: "https://xsrr-l2ye-dpbj.f2.xano.io/api:Coq7oZJp/posts/get_single/{post_id}"
       }
-      // {
-      //     pattern: "/team/profile/[^/]+",
-      //     metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
-      // }
       // Add more patterns and their metadata endpoints as needed
   ]
 };
